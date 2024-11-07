d1cfb9d38d0f97e3c8af260400cfe2ff45f8a7f1
DanzoLuvsKFC <131270351+DanzoLuvsKFC@users.noreply.github.com>
2024-11-07 15:20:22 +0200

    another test bro

--- Scripts/footer.js
+++ Scripts/footer.js
@@ -40,7 +40,7 @@ window.onload = () => {
         },
         { 
             link: "https://www.tiktok.com/@lamborghini?is_from_webapp=1&sender_device=pc", 
-            imgSrc: "/Commercial-Lambo/Images/tiktok.png",  
+            imgSrc: "/Commercial-Lambo/Images/tik.png",  
             altText: "Tiktok"
         }
         
